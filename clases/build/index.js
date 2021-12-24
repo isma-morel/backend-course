5fffed59d027bdf498b72a42afa17dbe407fadb2
Ismael Morel <ismaelmorel84@gmail.com>
2021-12-23 22:51:51 -0300

    f

---
+++ clases/build/index.js
@@ -0,0 +1,35 @@
+"use strict";
+class User {
+    constructor(name, lastName, pet) {
+        this.name = name;
+        this.lastName = lastName;
+        this.books = [];
+        this.pets = [pet];
+    }
+    getFullName() {
+        return `${this.name} ${this.lastName}`;
+    }
+    addPet(pet) {
+        this.pets.push(pet);
+    }
+    countPets() {
+        return this.pets.length;
+    }
+    addBook(name, autor) {
+        const book = {
+            name: name,
+            autor: autor,
+        };
+        this.books.push(book);
+    }
+    getBookNames() {
+        return this.books;
+    }
+}
+const user = new User("Ismael", "Morel", "gato");
+console.log(user.getFullName());
+user.addPet("perro");
+console.log(user.countPets());
+user.addBook("La vida invisible", "Addie Larue");
+user.addBook("La vida invisible 2", "Addie Larue");
+console.log(user.getBookNames());
---
+++ clases/index.ts
@@ -0,0 +1,49 @@
+interface Book {
+  name: string;
+  autor: string;
+}
+
+class User {
+  name: string;
+  lastName: string;
+  books: Book[];
+  pets: string[];
+  constructor(name: string, lastName: string, pet: string) {
+    this.name = name;
+    this.lastName = lastName;
+    this.books = [];
+    this.pets = [pet];
+  }
+  getFullName(): string {
+    return `${this.name} ${this.lastName}`;
+  }
+
+  addPet(pet: string): void {
+    this.pets.push(pet);
+  }
+  countPets(): number {
+    return this.pets.length;
+  }
+  addBook(name: string, autor: string): void {
+    const book: Book = {
+      name: name,
+      autor: autor,
+    };
+    this.books.push(book);
+  }
+  getBookNames(): Book[] {
+    return this.books;
+  }
+}
+
+const user: User = new User("Ismael", "Morel", "gato");
+
+console.log(user.getFullName());
+user.addPet("perro");
+console.log(user.countPets());
+
+user.addBook("La vida invisible", "Addie Larue");
+
+user.addBook("La vida invisible 2", "Addie Larue");
+
+console.log(user.getBookNames());
